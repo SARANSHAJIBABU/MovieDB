6152f3ea1b664f8b4e270b5de91f2744642768ea
saran <saranbs2013@gmail.com>
2016-11-01 18:10:51 +0800

    Added constants

---
+++ app/src/main/java/com/saran/mdb/Constants.java
@@ -0,0 +1,10 @@
+package com.saran.mdb;
+
+/**
+ * Created by Saran on 1/11/16.
+ */
+
+public class Constants {
+    public static final String BASE_URL ="https://api.themoviedb.org/3";
+    public static final String API_KEY = "58f45bae299a6a8069ff7ae04ea33793";
+}
