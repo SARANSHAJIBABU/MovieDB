20ef1b03bc7a9368501363ac5447284e03041d24
saran <saranbs2013@gmail.com>
2016-11-02 15:15:51 +0800

    Added log interceptor for okhttp client

--- app/src/main/java/com/saran/mdb/ApiCLient.java
+++ app/src/main/java/com/saran/mdb/ApiCLient.java
@@ -1,6 +1,9 @@
 package com.saran.mdb;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by Saran on 2/11/16.
@@ -11,9 +14,16 @@ public class ApiCLient {
 
     public static Retrofit getClient(){
         if(retrofit==null){
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(Constants.BASE_URL)
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addNetworkInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
                     .build();
+
+                retrofit = new Retrofit.Builder()
+                        .baseUrl(Constants.BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .client(client)
+                        .build();
         }
         return retrofit;
     }
