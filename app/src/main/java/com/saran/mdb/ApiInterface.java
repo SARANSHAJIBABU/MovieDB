caa65ede213dec50640a4b369ea8c8eae5555a15
saran <saranbs2013@gmail.com>
2016-11-07 22:38:24 +0800

    Added movie details api

--- app/src/main/java/com/saran/mdb/ApiInterface.java
+++ app/src/main/java/com/saran/mdb/ApiInterface.java
@@ -2,6 +2,7 @@ package com.saran.mdb;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 /**
@@ -12,5 +13,8 @@ public interface ApiInterface {
 
     @GET("movie/now_playing")
     Call<LatestMovies> getTopLatestMovies(@Query("api_key")String apiKey, @Query("language") String language);
+
+    @GET("movie/{movie_id}")
+    Call<LatestMovies.Result> getMovieDetails(@Path("movie_id") String movieId,@Query("api_key")String apiKey, @Query("language") String language);
 
 }
